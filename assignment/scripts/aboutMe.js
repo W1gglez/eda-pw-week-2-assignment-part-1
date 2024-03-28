02a67cf1f3907b3ef702bc2d2f80fdbfbcf03aa6
lweigel <snr.wiggels@gmail.com>
2024-03-28 13:59:05 -0500

    Complete examples 1-5

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,21 +1,28 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Logan';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Weigel';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
+let fullName = firstName + ' ' + lastName;
 
+console.log(fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
+let luckyNumber = 22;
 
+console.log(luckyNumber);
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
+let introduction =
+  'My name is ' + fullName + ', and I think ' + luckyNumber + ' is a winner!';
 
+console.log(introduction);
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 //     Console log the value of `adventurous`
 
@@ -32,32 +39,31 @@ let firstName = '';
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
 // 12 - Create a variable called `result`. Create a conditional:
-//      if adventurous is true, set `result` to be "Adventures are great!", 
+//      if adventurous is true, set `result` to be "Adventures are great!",
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
 
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
-//      Create a compound conditional: 
+//      Create a compound conditional:
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
 
-// 14 - Create a variable called `petStatus`. 
+// 14 - Create a variable called `petStatus`.
 //      Write a conditional that covers the following:
 //      if the value of `pets` is less than the value of `allowedPets`,
-//      set `petStatus` to the value of "I can have more pets" 
+//      set `petStatus` to the value of "I can have more pets"
 //      if the value of `pets` is equal to the value of `allowedPets`,
-//      set `petStatus` to the value of "I have enough pets" 
+//      set `petStatus` to the value of "I have enough pets"
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
-
 
 // STRETCH GOALS:
 
 // 15 - Make a variable called `mostPets` and a conditional that
 //      correctly checks the `pets` and `friendsPets` variables, and
 //      assigns the highest value to `mostPets`. There's several possibilities --
-//      be sure to think through all the scenarios. 
+//      be sure to think through all the scenarios.
 //      console.log `mostPets` after the conditional has run.
 
 // 16 - Make a variable called `luckyResult`
@@ -71,16 +77,6 @@ let firstName = '';
 
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
 
-
-
-
-
-
-
-
-
-
-
 // DO NOT MODIFY
 // Used for automated testing
 try {
@@ -89,7 +85,8 @@ try {
     lastName: typeof lastName !== 'undefined' ? lastName : undefined,
     fullName: typeof fullName !== 'undefined' ? fullName : undefined,
     luckyNumber: typeof luckyNumber !== 'undefined' ? luckyNumber : undefined,
-    introduction: typeof introduction !== 'undefined' ? introduction : undefined,
+    introduction:
+      typeof introduction !== 'undefined' ? introduction : undefined,
     adventurous: typeof adventurous !== 'undefined' ? adventurous : undefined,
     food: typeof food !== 'undefined' ? food : undefined,
     pets: typeof pets !== 'undefined' ? pets : undefined,
